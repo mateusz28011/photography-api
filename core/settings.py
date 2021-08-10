a660879a79d41f80d6f9eb2cb3d0da3e8b17a48d
mateusz28011 <mathew28011@gmail.com>
2021-08-10 23:57:46 +0200

    Setting up pagination

--- core/settings.py
+++ core/settings.py
@@ -185,6 +185,8 @@ REST_FRAMEWORK = {
         # "rest_framework.authentication.TokenAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 25,
 }
 
 SIMPLE_JWT = {
